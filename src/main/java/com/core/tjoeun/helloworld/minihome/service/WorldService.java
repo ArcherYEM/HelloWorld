fa7675e4a63385699502b341a830effd467279ee
admin <admin@DESKTOP-H4I1UJ5>
2023-12-20 12:04:52 +0900

    패키지 구조 변경 커밋

--- src/main/java/com/core/tjoeun/helloworld/minihome/service/WorldService.java
+++
@@ -1,5 +0,0 @@
-package com.core.tjoeun.helloworld.minihome.service;
-
-public interface WorldService {
-
-}
--- src/main/java/com/core/tjoeun/mnHome/main/service/WorldServiceImpl.java
+++ src/main/java/com/core/tjoeun/mnHome/main/service/WorldServiceImpl.java
@@ -1,14 +1,14 @@
-package com.core.tjoeun.helloworld.minihome.service;
+package com.core.tjoeun.mnHome.main.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.core.tjoeun.helloworld.minihome.dao.WorldDao;
+import com.core.tjoeun.mnHome.main.dao.WorldDao;
+
 
 @Service
 public class WorldServiceImpl implements WorldService{
 
 	@Autowired
-	WorldDao worldDao;
-	
+	WorldDao WorldDao;
 }
