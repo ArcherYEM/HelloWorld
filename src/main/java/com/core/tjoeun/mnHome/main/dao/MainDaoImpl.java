c8cf45f24b3060ab48d6fe584ee5ca668396a4ac
jinwoo <jinwoo4506@gmail.com>
2024-01-02 01:43:59 +0900

    miniroom

--- src/main/java/com/core/tjoeun/mnHome/main/dao/MainDaoImpl.java
+++ src/main/java/com/core/tjoeun/mnHome/main/dao/MainDaoImpl.java
@@ -30,4 +30,52 @@ public class MainDaoImpl implements MainDao{
 		return mapper.getProfileHistory(userNickname);
 	}
 
+	@Override
+	public List<Map> getMinimi() {
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);
+		
+		return mapper.getMinimi();
+	}
+
+	@Override
+	public void resetMinimi(String userNickname) {		
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);	
+		
+		mapper.resetMinimi(userNickname);
+	}
+
+	@Override
+	public void insertMinimi(Map<String, Object> minimiData) {
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);	
+		
+		mapper.insertMinimi(minimiData);		
+	}
+
+	@Override
+	public void upsertBackground(Map<String, Object> params) {		
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);	
+		
+		mapper.upsertBackground(params);		
+	}
+	
+	@Override
+	public void resetBackground(String userNickname) {		
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);	
+		
+		mapper.resetBackground(userNickname);
+	}
+
+	@Override
+	public List<Map> selectMinimi(String userNickname) {
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);
+		
+		return mapper.selectMinimi(userNickname);
+	}
+
+	@Override
+	public Map selectBackground(String userNickname) {
+		MainMapper mapper = sqlSession.getMapper(MainMapper.class);
+		
+		return mapper.selectBackground(userNickname);
+	}
 }
