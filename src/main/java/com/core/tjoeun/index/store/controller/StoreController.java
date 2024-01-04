3c430c57bd36909ab16b700662fe2340d92be95e
admin <admin@DESKTOP-H4I1UJ5>
2024-01-04 17:30:50 +0900

    장바구니 기능 1차 커밋

--- src/main/java/com/core/tjoeun/index/store/controller/StoreController.java
+++ src/main/java/com/core/tjoeun/index/store/controller/StoreController.java
@@ -4,15 +4,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.core.tjoeun.index.store.service.StoreService;
+import com.core.tjoeun.util.CartItem;
+import com.core.tjoeun.util.ShoppingCart;
 
 @Controller
 public class StoreController {
@@ -93,5 +100,28 @@ public class StoreController {
 		return "/store/order";
 	}
 
+	@GetMapping("/store/loadCart")
+	@ResponseBody
+    public List<CartItem> loadCart(HttpSession session) {
+        ShoppingCart shoppingCart = getOrCreateShoppingCart(session);
+        return shoppingCart.getCartItems();
+    }
+
+    @PostMapping("/store/addToCart")
+    @ResponseBody
+    public void addToCart(@RequestBody CartItem item, HttpSession session) {
+        ShoppingCart shoppingCart = getOrCreateShoppingCart(session);
+        shoppingCart.addToCart(item);
+    }
+
+    private ShoppingCart getOrCreateShoppingCart(HttpSession session) {
+        ShoppingCart shoppingCart = (ShoppingCart) session.getAttribute("cart");
+        if (shoppingCart == null) {
+            shoppingCart = new ShoppingCart();
+            session.setAttribute("cart", shoppingCart);
+        }
+        return shoppingCart;	
+    }
+	
 	
 }
