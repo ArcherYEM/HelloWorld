5f5d44afbd49e72fd545178ce90bbc1078269705
jinwoo <jinwoo4506@gmail.com>
2024-01-05 10:31:37 +0900

    aa

--- src/main/java/com/core/tjoeun/index/store/service/StoreService.java
+++ src/main/java/com/core/tjoeun/index/store/service/StoreService.java
@@ -6,9 +6,7 @@ import java.util.Map;
 public interface StoreService {
 
 	public List<Map> getBgmList(Map map) throws Exception;
-	
-	public List<Map> searchBgm(Map map) throws Exception;
-	
+		
 	public int putBgm(Map map) throws Exception;
 	
 	public long selectStoreCnt(Map map);
