24a001923e897e4a9ad550a8c9cf3e4e9212706e
jinwoo <jinwoo4506@gmail.com>
2024-01-09 14:09:45 +0900

    a

--- src/main/webapp/resources/js/diary.js
+++ src/main/webapp/resources/js/diary.js
@@ -1,19 +1,25 @@
-$(document).on('click', '#btnBoardWrite', function() {
-    console.log("버튼 클릭됨");
-    document.getElementById('diaryHiddenTitle').value = document.getElementById('diaryTitle').value;
-
-    console.log(document.getElementById('diaryHiddenUserNickname').value);
-
-    // 에디터 초기화 확인
-    if (oEditors.length === 0) {
+ // 에디터 초기화 확인
+if (oEditors.length === 0) {
         initializeEditor(); // 초기화되지 않았다면 초기화
     }
 
     oEditors.getById["txtContent"].exec("UPDATE_CONTENTS_FIELD", []);
-
     let content = document.getElementById("txtContent").value.replace("\r\n", "");
 
-    document.getElementById('diaryHiddenContent').value = content;
-
     document.getElementById('frmDiary').submit();
-});
+    
+function addDiary() {
+    console.log("버튼 클릭됨");
+    
+    let userNickname = $("#hiddenUserNickname").val();
+    let title = $("#diaryTitle").val();
+    console.log(userNickname);
+    console.log(title);
+    console.log(txtContent);
+    console.log(content);
+    
+    document.getElementById('diaryHiddenTitle').value = document.getElementById('diaryTitle').value;
+
+    console.log(document.getElementById('diaryHiddenUserNickname').value);
+};
+
