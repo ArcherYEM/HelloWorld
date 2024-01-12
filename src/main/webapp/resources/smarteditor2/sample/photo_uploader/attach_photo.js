c11f8a9df92376e748bb8e1cd5e0c3782413cb10
jeongeun <jjeongeun327@gmail.com>
2024-01-12 14:46:47 +0900

    게시판 사진 등록 포기

--- src/main/webapp/resources/smarteditor2/sample/photo_uploader/attach_photo.js
+++ src/main/webapp/resources/smarteditor2/sample/photo_uploader/attach_photo.js
@@ -334,7 +334,8 @@
     	var tempFile,
     		sUploadURL;
     	
-    	sUploadURL= '/smarteditorMultiImageUpload'; 	//upload URL
+//    	sUploadURL= '/smarteditorMultiImageUpload'; 	//upload URL
+    	sUploadURL= '/mnHome/imgUpload'; 	//upload URL
     	
     	//파일을 하나씩 보내고, 결과를 받음.
     	for(var j=0, k=0; j < nImageInfoCnt; j++) {
@@ -478,7 +479,8 @@
  	 */
  	function callFileUploader (){
  		oFileUploader = new jindo.FileUploader(jindo.$("uploadInputBox"),{
- 			sUrl  : location.href.replace(/\/[^\/]*$/, '') + '/file_uploader.php',	//샘플 URL입니다.
+ //			sUrl  : location.href.replace(/\/[^\/]*$/, '') + '/file_uploader.php',	//샘플 URL입니다.
+ 			sUrl  : location.href.replace(/\/[^\/]*$/, '') + '/mnHome/imgUpload',	//샘플 URL입니다.
  	        sCallback : location.href.replace(/\/[^\/]*$/, '') + '/callback.html',	//업로드 이후에 iframe이 redirect될 콜백페이지의 주소
  	    	sFiletype : "*.jpg;*.png;*.bmp;*.gif",						//허용할 파일의 형식. ex) "*", "*.*", "*.jpg", 구분자(;)	
  	    	sMsgNotAllowedExt : 'JPG, GIF, PNG, BMP 확장자만 가능합니다',	//허용할 파일의 형식이 아닌경우에 띄워주는 경고창의 문구
