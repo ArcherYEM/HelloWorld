7f59891eac04b36aeac70efe143b9e1721a89709
yangem <em.yang94@gmail.com>
2024-01-25 13:55:38 +0900

    visibility 구현

--- src/main/java/com/core/tjoeun/mnHome/album/service/AlbumServiceImpl.java
+++ src/main/java/com/core/tjoeun/mnHome/album/service/AlbumServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.apache.commons.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
@@ -21,70 +20,79 @@ import com.core.tjoeun.mnHome.album.dao.AlbumDao;
 
 @Service
 @EnableTransactionManagement
-public class AlbumServiceImpl implements AlbumService {
-
+public class AlbumServiceImpl implements AlbumService{
+	
 	@Value("${file.upload.path}")
-	private String fileUploadPath;
+    private String fileUploadPath;
 
 	@Autowired
 	AlbumDao albumDao;
-
+	
 	@Override
 	@Transactional(readOnly = false, rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
-	@CacheEvict(key = "#map['userNickname']", value = "currentContent")
+	@CacheEvict(key="#map['userNickname']", value="currentContent")
 	public void insertAlbum(MultipartFile[] uploadFile, Map map) throws Exception {
-
-		// 사진 업로드
+		
+		//사진  업로드
 		String o_filename;
-		String n_filename;
-		String filename;
-		List<String> filenames = new ArrayList<>();
-
-		for (MultipartFile multipartFile : uploadFile) {
+        String n_filename;
+        String filename;
+        List<String> filenames = new ArrayList<>();
+		
+		for(MultipartFile multipartFile : uploadFile) {
+//			Log.info("--------------------------------");
+//			Log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+//			Log.info("Upload File Size : " + multipartFile.getSize());
+			
 			o_filename = multipartFile.getOriginalFilename();
 			n_filename = UUID.randomUUID().toString() + "-" + o_filename;
 			filename = n_filename;
-
+			
 			File savefile = new File(fileUploadPath + File.separator + n_filename);
 			try {
-	            multipartFile.transferTo(savefile);
-	        } catch (Exception e) {
-	            throw new FileUploadException("파일 업로드 중 오류 발생", e);
-	        }
+				multipartFile.transferTo(savefile);
+				filenames.add(filename);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
 		}
-
+		
 		StringBuilder stringBuilder = new StringBuilder();
 		for (String str : filenames) {
-			if (stringBuilder.length() > 0) {
-				stringBuilder.append(",");
-			}
-			stringBuilder.append(str);
-		}
-
-		// 업로드된 파일 이름을 쉼표로 구분하여 넣음
+            if (stringBuilder.length() > 0) {
+            	stringBuilder.append(","); // 쉼표 추가
+            }
+            stringBuilder.append(str);
+        }
+		
+		//업로드된 파일 이름을 쉼표로 구분하여 넣음 
 		map.put("imagePath", stringBuilder.toString());
-
+		
 		int result = albumDao.insertAlbum(map);
-		if (result != 1) {
-			throw new DatabaseInsertException("DB에 앨범 정보 삽입 중 오류 발생");
+		
+		if(result != 1) {
+			throw new Exception();
 		}
+		
 	}
-
+	
 	@Override
 	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
 	public List<HashMap> getAlbum(Map map) {
-
+		
 		return albumDao.selectAlbum(map);
 	}
 
 	@Override
 	@Transactional(readOnly = false, rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
-	@CacheEvict(value = "currentContent")
+	@CacheEvict(value="currentContent")
 	public void updateAlbum(Map map) throws Exception {
 		int result = albumDao.updateAlbum(map);
-		if (result != 1) {
+		if(result != 1) {
 			throw new Exception();
 		}
+		
 	}
 	
 }
